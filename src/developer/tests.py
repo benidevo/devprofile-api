cc4882166bb8710c1e4f47d1fb6b64b472027311
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-22 00:56:53 +0100

    setup test

--- src/developer/tests.py
+++ src/developer/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SampleTestCase(TestCase):
+  def test_addition(self):
+    one = 1
+    two = 2
+    self.assertEqual(one + two, 3)
