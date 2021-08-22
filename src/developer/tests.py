3d04210b03b4a221131d137fcbfba086ab94ecdc
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-22 01:15:10 +0100

    update CI

--- src/developer/tests.py
+++ src/developer/tests.py
@@ -3,6 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 class SampleTestCase(TestCase):
   def test_addition(self):
-    one = 1
-    two = 2
-    self.assertEqual(one + two, 3)
+    self.assertEqual(1+2, 3)
