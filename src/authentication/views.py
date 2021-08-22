f0b6a439f43a787ceb98448058f16609cd2488a1
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-22 23:09:20 +0100

    add test route

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views  import APIView
+from utils.response import Response
 
 # Create your views here.
+
+class TestView(APIView):
+  def get(self, request):
+    return Response(data={'news': 'welcome to api'}, status=status.HTTP_200_OK)
